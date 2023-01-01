cc9c49b71c634ff84ce3c6523b9cf63442e891b3
Aqib Mahmud Rime <akibmahmudrime@gmail.com>
2023-01-01 23:16:36 +0600

    modify loggedInInfo.ts

--- src/pages/jotaiStore/loggedInInfo.ts
+++ src/pages/jotaiStore/loggedInInfo.ts
@@ -2,6 +2,5 @@ import { atom } from 'jotai'
 import crypto from "crypto"
 
 export const isLoggedIn = atom(true)
-// export const currentUserId = atom(uuid.toString()) // TODO : we may need to get it from data
-export const currentUserId = atom(crypto.randomBytes(20).toString('hex'))
+export const currentUserId = atom(crypto.randomBytes(20).toString('hex')) // TODO : we may need to get it from data
 
